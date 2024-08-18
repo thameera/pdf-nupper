dae58a7cbac32a30582bddb32cad22fa2951570c
Thameera Senanayaka <me@thameera.com>
2024-08-18 19:33:36 +1000

    Support landscape mode

--- pdf_combine/cli.py
+++ pdf_combine/cli.py
@@ -5,10 +5,12 @@ def main():
     parser = argparse.ArgumentParser(description="N-up PDF pages")
     parser.add_argument("input_pdf", help="Input PDF file")
     parser.add_argument("output_pdf", help="Output PDF file")
+    parser.add_argument("-l", "--landscape", action="store_true", help="Use landscape mode instead of portrait mode")
+
 
     args = parser.parse_args()
 
-    combine(args.input_pdf, args.output_pdf)
+    combine(args.input_pdf, args.output_pdf, args.landscape)
 
 if __name__ == "__main__":
     main()
--- pdf_combine/core.py
+++ pdf_combine/core.py
@@ -1,31 +1,38 @@
 from pypdf import PdfWriter, PdfReader
 from pypdf.generic import RectangleObject
 
-def combine(input_pdf_path, output_pdf_path):
+A4_WIDTH = 595.28  # A4 width in points
+A4_HEIGHT = 841.89  # A4 height in points
+
+def combine(input_pdf_path, output_pdf_path, is_landscape=False):
     reader = PdfReader(input_pdf_path)
     writer = PdfWriter()
 
     num_pages = len(reader.pages)
-    a4_width = 595.28  # A4 width in points
-    a4_height = 841.89  # A4 height in points
+    if is_landscape:
+        width = A4_HEIGHT
+        height = A4_WIDTH
+    else:
+        width = A4_WIDTH
+        height = A4_HEIGHT
 
     for i in range(0, num_pages, 2):
-        new_page = writer.add_blank_page(width=a4_width, height=a4_height)
+        new_page = writer.add_blank_page(width=width, height=height)
 
         positions = [
             (0, 0),  # Left side
-            (a4_width / 2, 0)  # Right side
+            (width / 2, 0)  # Right side
         ]
 
         for j in range(2):
             if i + j < num_pages:
                 page = reader.pages[i + j]
-                page.scale_by(a4_width / page.mediabox.width)
+                page.scale_by(width / page.mediabox.width)
 
                 # Translate the page to the correct position
                 position = positions[j]
                 translation_matrix = [1, 0, 0, 1, position[0], position[1]]
-                page.mediabox = RectangleObject([0, 0, a4_width / 2, a4_height])
+                page.mediabox = RectangleObject([0, 0, width / 2, height])
                 new_page.merge_transformed_page(page, translation_matrix)
 
     with open(output_pdf_path, "wb") as output_file:
